08a44ecc3440242c53de9b5d17c3f61c5df47e38
Blessing Mbalaka <132813484+Blessing-Mbalaka@users.noreply.github.com>
2026-04-11 03:52:35 +0200

    Set default values for Employee properties
    Initialize Name, Surname, Email, and JobTitle properties to avoid null reference errors.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,6 +3,7 @@ namespace API_Endpoints_Salary_Calculator.Models
     public class Employee
     {
         public int Id { get; set; }
+        //Just covering our asses with string.Empty so we can reduce null reference errors. 
         public string Name { get; set; } = string.Empty;
         public string Surname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
